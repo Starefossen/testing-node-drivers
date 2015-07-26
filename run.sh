b41263e61a366212f951864faef8faf1342ca46b
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-07-26 22:45:03 +0200

    Only output test logs if something fails

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+docker-compose stop
+docker-compose rm -fv
+
 for D in *; do
   if [ -d "${D}" ]; then
-    docker-compose run --rm "${D}" npm install
-    docker-compose run --rm "${D}" npm test
+    echo "[${D}] installing dependencies..."
+    LOG=$(docker-compose run --rm "${D}" npm install)
+    sleep 2
+
+    if [ "$?" -ne "0" ]; then
+      echo "${LOG}";
+    fi
+
+    echo "[${D}] running tests..."
+    LOG=$(docker-compose run --rm "${D}")
+
+    if [ "$?" -ne "0" ]; then
+      echo "${LOG}";
+    fi
   fi
 done
